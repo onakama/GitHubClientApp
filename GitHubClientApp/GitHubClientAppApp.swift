775d42a6f36a78a487c128c8263553d90bab12df
taisei <ninzin60en.dev@gmail.com>
2022-07-18 01:08:55 +0900

    Initial Commit

---
+++ GitHubClientApp/GitHubClientAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubClientAppApp.swift
+//  GitHubClientApp
+//
+//  Created by onakama on 2022/07/18.
+//
+
+import SwiftUI
+
+@main
+struct GitHubClientAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
