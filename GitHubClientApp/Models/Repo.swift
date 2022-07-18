639e3ddc4cc0b9171aadbc0478990fbf03d9514e
taisei <ninzin60en.dev@gmail.com>
2022-07-18 22:41:47 +0900

    add-2.2

--- GitHubClientApp/Models/Repo.swift
+++ GitHubClientApp/Models/Repo.swift
@@ -7,10 +7,10 @@
 
 import Foundation
 
-struct Repo: Identifiable {
+struct Repo: Identifiable, Codable {
     var id: Int
     var name: String
     var owner: User
-    var description: String
+    var description: String?
     var stargazersCount: Int
 }
